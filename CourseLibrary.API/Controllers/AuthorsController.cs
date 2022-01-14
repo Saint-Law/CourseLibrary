4ddfd6eac546af9f07f62d870dd5b09beeae35ab
Saintlaw <Saintlaw@DESKTOP-7FF550F>
2022-01-14 02:44:14 +0100

    Add project files.

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,41 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return Ok(authorsFromRepo);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            if (!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+            return Ok(authorFromRepo);
+        }
+    }
+}
